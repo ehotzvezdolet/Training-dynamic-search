024c857556a9e8281195979a9940ca39933d2175
Igorek <ehotzvezdolet@gmail.com>
2023-11-24 23:04:48 +0100

    solution this exercise

--- code.js
+++ code.js
@@ -1,8 +1,32 @@
-const api = 
-`https://gist.githubusercontent.com/VasilyMur
-/43ef6df83bba694f871f11a16ed7556d/raw/
-b6edff674e35452d6c57ec64177a558f7adb432e/moscowSubway.json`;
+const api = `api.json`;
 
 const stations = [];
 
-console.log('hello!');
+fetch(api).then(res => res.json()).then(data => {
+ data.forEach(line => {
+  stations.push(...line.stations)
+ })
+})
+
+const searchInput = document.querySelector('.search');
+const searchOptions = document.querySelector('.options');
+
+function getOptions(word, stations) {
+ return stations.filter(s => {
+  const regex = new RegExp(word, 'gi');
+  return s.name.match(regex);
+ })
+}
+
+function showOptions() {
+ const result = getOptions(this.value, stations);
+ const html = result.map(station => {
+  const regex = new RegExp(this.value, 'gi');
+  const newName = station.name.replace(regex, `<span class="hl">${this.value}</span>`);
+  return `<li><span>${newName}</span></li>`
+ }).slice(0, 7).join('');
+ searchOptions.innerHTML = this.value ? html : null;
+}
+
+searchInput.addEventListener('change', showOptions);
+searchInput.addEventListener('keyup', showOptions);
